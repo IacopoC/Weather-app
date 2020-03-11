92edbef630be62963fff502799ef937567cf2599
iacopo <iacopo.cutino@gmail.com>
2020-03-11 20:49:34 +0100

    Modifica e aggiunta dati meteo in homepage

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -21,15 +21,29 @@
     <div class="container">
 
         <div class="row">
-            <div class="col-md-8 mb-5">
+            <div class="col-md-12 mb-5">
                 <input class="form-control" type="text" placeholder="Search" aria-label="Search">
                 <hr>
-                Timezone: {{ $weather_data->timezone }}
+                <div class="pl-4">
+                    <p>Timezone: {{ $weather_data->timezone }}</p>
+                </div>
                 <div class="mt-4 mb-4">
-                    <div class="bg-light p-4">
+                    <div class="p-4">
                         <h3> Weather now:</h3>
-                            <h4>Summary of the day: {{ $weather_data->currently->summary }}</h4>
-                            <p>{{ $weather_data->currently->icon }}</p>
+                            <p>Summary of today: {{ $weather_data->currently->summary }}</p>
+                        <hr>
+                        <div class="row">
+                        <div class="col-md-6">
+                        <p>Rain probability: {{ $weather_data->currently->precipProbability }} %</p>
+                        <p>Temp: {{ $weather_data->currently->temperature }} C°</p>
+                         <p>Uv Index: {{ $weather_data->currently->uvIndex }}</p>
+                        </div>
+                        <div class="col-md-6">
+                            <p>Humidity: {{ $weather_data->currently->humidity }} %</p>
+                            <p>Pressure: {{ $weather_data->currently->pressure }} mb</p>
+                            <p>Visibility: {{ $weather_data->currently->uvIndex }}</p>
+                        </div>
+                        </div>
                     </div>
                 </div>
                 <div class="p-4">
@@ -37,15 +51,10 @@
                     <p>Summary of the week: {{ $weather_data->daily->summary }}</p>
                 <hr>
                 @foreach($weather_data->daily->data as $weather_day)
-                    <p>Time: {{ $weather_day->time }}</p>
-                    <p>Summary: {{ $weather_day->summary }}</p>
-                        <p>{{ $weather_day->icon }}</p>
+                    <p>{{ $weather_day->summary }}</p>
                     <hr>
                     @endforeach
                 </div>
-            </div>
-            <div class="col-md-4 mb-5">
-
             </div>
         </div>
         <!-- /.row -->
