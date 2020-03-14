1a2fb35237e02049099cf27dfa803eca5e06df11
iacopo <iacopo.cutino@gmail.com>
2020-03-14 15:16:54 +0100

    Modifica formato date e layout home

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -11,7 +11,11 @@
             <div class="row h-100 align-items-center">
                 <div class="col-lg-12">
                     <h1 class="display-4 text-white mt-5 mb-2">Benvenuto su Weather app</h1>
-                    <p class="lead mb-5 text-white-50">Scopri che tempo fa nella tua zona</p>
+                    <p class="lead mb-5 text-white-50">Scopri che tempo fa nella <a class="text-white" href="#">tua zona</a></p>
+                    <form class="form-inline mt-2 mt-md-0">
+                        <input class="form-control mr-sm-2" type="text" placeholder="Search" aria-label="Search">
+                        <button class="btn btn-outline-light my-2 my-sm-0" type="submit">Search</button>
+                    </form>
                 </div>
             </div>
         </div>
@@ -22,9 +26,7 @@
 
         <div class="row">
             <div class="col-md-12 mb-5">
-                <input class="form-control" type="text" placeholder="Search" aria-label="Search">
-                <hr>
-                <div class="pl-4">
+                <div class="pt-2 pl-4">
                     <p>Timezone: {{ $weather_data->timezone }}</p>
                 </div>
                 <div class="mt-4 mb-4">
@@ -51,7 +53,18 @@
                     <p>Summary of the week: {{ $weather_data->daily->summary }}</p>
                 <hr>
                 @foreach($weather_data->daily->data as $weather_day)
-                    <p>{{ $weather_day->summary }}</p>
+                        <p class="pb-2"><strong>{{ gmdate("d-m-Y", $weather_day->time) }}</strong></p>
+                        <p>{{ $weather_day->summary }}</p>
+                        <div class="row">
+                            <div class="col-md-6">
+                                <p>Rain probability: {{ $weather_day->precipProbability }} %</p>
+                                <p>Temp: {{ $weather_day->temperatureMin }} / {{ $weather_day->temperatureMax }} C°</p>
+                            </div>
+                            <div class="col-md-6">
+                                <p>Humidity: {{ $weather_day->humidity }} %</p>
+                                <p>Pressure: {{ $weather_day->pressure }} mb</p>
+                            </div>
+                        </div>
                     <hr>
                     @endforeach
                 </div>
