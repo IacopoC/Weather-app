a83f90e42bbd197241a91cf8315d35b97c0bb2e4
iacopo <iacopo.cutino@gmail.com>
2020-09-13 21:31:20 +0200

    Aggiunto salvataggio località controller

--- app/Http/Controllers/GeneralController.php
+++ app/Http/Controllers/GeneralController.php
@@ -1,9 +1,8 @@
 <?php
 
 namespace App\Http\Controllers;
-
+use Illuminate\Support\Facades\Auth;
 use App\Location;
-
 use Illuminate\Http\Request;
 
 class GeneralController extends Controller
@@ -28,8 +27,24 @@ class GeneralController extends Controller
         $query = $request->input('q');
         $weather_results = $this->basetype->searchWeather($query);
 
+        $this->storeLocation($query);
+
         return view('search', compact('weather_results','query'));
     }
 
 
-}
+    public function storeLocation($query) {
+
+        if (Auth::check()) {
+            $id = Auth::user()->id;
+
+            $locations = new Location;
+
+            $locations->location = $query;
+            $locations->user_id = $id;
+
+            $locations->save();
+        }
+    }
+
+    }
