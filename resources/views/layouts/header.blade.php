08ed524f9c0439f73f4adf0cc55d4dcb9f30c7c9
39340 <iacopo.cutino@gmail.com>
2020-11-03 20:41:11 +0100

    aggiunto favicon

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -4,7 +4,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <meta name="description" content="">
     <meta name="author" content="">
-
+    <link rel='icon' href='{{ asset('img/favicon.png') }}' type='image/x-icon' sizes="16x16" />
     <title>@yield('title')</title>
 
     <!-- Bootstrap core CSS -->
